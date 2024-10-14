2f8b533569f540c708496d8a4bfd33f35fa8cc05
Caroline Mimeault <carolinemimeault@gmail.com>
2024-10-14 13:23:13 -0400

    resolve actings

--- scripts/index.py
+++ scripts/index.py
@@ -6,7 +6,7 @@ from datetime import datetime, date
 output = {}
 
 
-def is_current_employee(data):
+def is_current_employee(row):
     """Identifies all current employees."""
     # if no end date, current employee, return True
     if row["endsOn"] == "":
@@ -19,11 +19,29 @@ def is_current_employee(data):
         return False
 
 
+def to_date(date_string):
+    """Change data type to date"""
+    return datetime.strptime(date_string, "%Y-%m-%d").date()
+
+
 with open("./data/raw/data.csv") as csvfile:
     data = csv.DictReader(csvfile, delimiter=",")
     for row in data:
         if is_current_employee(row):
-            output[row["employeeID"]] = row
-
+            # have I seen this employee before?
+            previous = output.get(row["employeeID"], None)
+            if previous == None:
+                # save information as never seen before
+                output[row["employeeID"]] = row
+            else:
+                # seen this employee before, need to decide which staffing action to keep
+                # select the most recent staffing action start date for employee
+                # get the start date of the current row
+                current_row_start_date = to_date(row["startsOn"])
+                # compare to start date of existing record for employee
+                # keep if start date for current is larger/later than previous
+                if current_row_start_date > to_date(previous["startsOn"]):
+                    output[row["employeeID"]] = row
 
 pp(output)
+pp(output.get("98754"))
