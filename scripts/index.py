f3beb97b21efed4b3ae33f2d4824c628624de823
Caroline Mimeault <carolinemimeault@gmail.com>
2024-10-13 22:38:44 -0400

    changed from pandas to python

--- scripts/index.py
+++ scripts/index.py
@@ -1,12 +1,27 @@
-# librairies
-import pandas as pd
+import csv
+from pprint import pp
+from datetime import datetime, date
 
-# data
-df = pd.read_csv("./data/raw/mock_data.csv", delimiter=",", header="infer")
+output = []
 
-# change some data types
-df["positionNumber"] = df["positionNumber"].astype("category")
-df["reportsToPositionNumber"] = df["reportsToPositionNumber"].astype("category")
 
-df.info()
-print(df.head())
+def current_employee(data):
+    """Identifies all current employees"""
+    # if no end date, current employee, return True
+    if row["endsOn"] == "":
+        return True
+    # if end date in the future, current employee, return True
+    end_date = datetime.strptime(row["endsOn"], "%Y-%m-%d").date()
+    if end_date > date.today():
+        return True
+    else:
+        return False
+
+
+with open("./data/raw/data.csv") as csvfile:
+    data = csv.DictReader(csvfile, delimiter=",")
+    for row in data:
+        if current_employee(row):
+            output.append(row)
+
+pp(output)
--- scripts/test.py
+++ scripts/test.py
@@ -3,12 +3,27 @@ from dagre_py.core import plot
 plot(
     {
         "nodes": [
-            {"label": "A"},
-            {"label": "B", "description": "This is an output node"},
+            {"label": "Christine"},
+            {"label": "Lucie"},
+            {"label": "Alex"},
+            {"label": "Nasila"},
+            {"label": "Ladan"},
+            {"label": "Marie"},
+            {"label": "Matt"},
+            {"label": "Asma"},
+            {"label": "Hocine"},
+            {"label": "Ann"},
         ],
         "edges": [
-            {"source": "A", "target": "B", "label": "edge1"},
-            {"source": "A", "target": "B", "label": "edge2"},
+            {"source": "Christine", "target": "Lucie"},
+            {"source": "Christine", "target": "Alex"},
+            {"source": "Lucie", "target": "Nasila"},
+            {"source": "Lucie", "target": "Ladan"},
+            {"source": "Lucie", "target": "Marie"},
+            {"source": "Lucie", "target": "Matt"},
+            {"source": "Alex", "target": "Asma"},
+            {"source": "Alex", "target": "Hocine"},
+            {"source": "Alex", "target": "Ann"},
         ],
     }
 )
