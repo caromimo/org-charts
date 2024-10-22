29cfc68cfa014e54209fc26aebf408ce0f91d5f9
Caroline Mimeault <carolinemimeault@gmail.com>
2024-10-21 21:21:09 -0400

    added interchange staffing action

--- automatedorgcharts/is_on_temporary_leave.py
+++ automatedorgcharts/is_on_temporary_leave.py
@@ -3,11 +3,12 @@ from .convert_to_date import convert_to_date
 
 
 # RESOLVE TEMPORARY LEAVES
-# identify assignmentOUT, secondmentOUT, maternity leave, paternity leave, parental leave, leave without pay (LWOP), sick LWOP (SLWOP)
+# identify assignmentOUT, secondmentOUT, interchange, maternity leave, paternity leave, parental leave, leave without pay (LWOP), sick LWOP (SLWOP)
 def is_on_temporary_leave(row):
     if (
         row["staffingActionType"] == "assignmentOUT"
         or row["staffingActionType"] == "secondmentOUT"
+        or row["staffingActionType"] == "interchange"
         or row["staffingActionType"] == "maternity leave"
         or row["staffingActionType"] == "paternity leave"
         or row["staffingActionType"] == "parental leave"
