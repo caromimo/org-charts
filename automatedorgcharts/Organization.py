6558f11d86aeba8a1f04bd0eb9e829b1052c843f
Caroline Mimeault <carolinemimeault@gmail.com>
2024-11-11 12:20:49 -0500

    processing different types of classification actions

--- automatedorgcharts/Organization.py
+++ automatedorgcharts/Organization.py
@@ -7,9 +7,21 @@ class Organization:
         self.positions = {}
 
     def process_action(self, action):
+
         if action["action"] == "classification":
-            # if type is create... (TODO other instances of classification)
-            self.positions[action["details"]["positionNumber"]] = action["details"]
+
+            if action["type"] == "create":
+                self.positions[action["details"]["positionNumber"]] = action["details"]
+
+            if action["type"] == "update":
+                self.positions[action["details"]["positionNumber"]] = {
+                    **self.positions[action["details"]["positionNumber"]],
+                    **action["details"],
+                }
+
+            if action["type"] == "delete":
+                del self.positions[action["details"]["positionNumber"]]
+
         if action["action"] == "staffing":
             # if type is appointment... (TODO other instances of staffing)
             self.employees[action["details"]["peopleSoftID"]] = action["details"]
