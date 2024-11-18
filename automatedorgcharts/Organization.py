1350d967e92a0093ffd093e671b80d8378045206
Caroline Mimeault <carolinemimeault@gmail.com>
2024-11-17 19:53:31 -0500

    visuals of the chart

--- automatedorgcharts/Organization.py
+++ automatedorgcharts/Organization.py
@@ -59,13 +59,46 @@ class Organization:
                 "label": "MDCCD Org Chart",
                 "splines": "ortho",
             },
-            node_attr={"shape": "box"},
+            node_attr={"shape": "none", "margin": "0"},
         )
 
         for position in self.positions:
             pp(self.positions[position])
             box = self.positions[position]
-            mdccd.node(str(box["positionNumber"]), box["title"])
+            incumbent_label = ""
+            if box.get("incumbent", False):
+                person = self.employees[box["incumbent"]]
+                incumbent_label = f"{person["firstName"]} {person["lastName"]}"
+            else:
+                incumbent_label = "VACANT"
+            label = f"""<
+            <TABLE BORDER = "1" 
+            BGCOLOR = "lightblue" 
+            CELLBORDER = "0" CELLPADDING = "1">
+            <TR>
+                <TD><FONT FACE = "arial"><B>{box["positionNumber"]}</B></FONT></TD>
+            </TR>
+            <TR>
+                <TD><FONT FACE = "arial">{incumbent_label}</FONT></TD>
+            </TR>
+            <TR>
+                <TD><FONT FACE = "arial">{box["title"]}</FONT></TD>
+            </TR>
+            <TR>
+                <TD><FONT FACE = "arial">{box["classification"]}</FONT></TD>
+            </TR>
+            <TR>
+                <TD><FONT FACE = "arial">{box["language"]}</FONT></TD>
+            </TR>
+            <TR>
+                <TD><FONT FACE = "arial">{box["security"]}</FONT></TD>
+            </TR>
+            <TR>
+                <TD><FONT FACE = "arial">{box["location"]}</FONT></TD>
+            </TR>
+            </TABLE>
+            >"""
+            mdccd.node(str(box["positionNumber"]), label=label)
             if box["reportsTo"]:
                 edges.append((str(box["positionNumber"]), str(box["reportsTo"])))
 
