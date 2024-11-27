94279ff975ed175d528253dcfb968fc4edeefb7e
Caroline Mimeault <carolinemimeault@gmail.com>
2024-11-26 23:38:01 -0500

    remove legacy code

--- automatedorgcharts/__init__.py
+++ automatedorgcharts/__init__.py
@@ -1,18 +1,8 @@
 from .convert_to_date import convert_to_date
-from .has_permanently_left import has_permanently_left
-from .is_current_employee import is_current_employee
-from .is_in_determinate_position import is_in_determinate_position
-from .is_in_indeterminate_position import is_in_indeterminate_position
-from .is_on_temporary_leave import is_on_temporary_leave
 from .Organization import Organization
 
 
 __all__ = [
     "convert_to_date",
-    "has_permanently_left",
-    "is_current_employee",
-    "is_in_determinate_position",
-    "is_in_indeterminate_position",
-    "is_on_temporary_leave",
     "Organization",
 ]
